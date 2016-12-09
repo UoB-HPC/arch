1e4d5cf6a4a70a0319d9f71f8273eccfc2c86d7e
Matt Martineau <m.martineau@hotmail.co.uk>
2016-12-09 12:22:00 +0000

    Adding files required for setting device

---
+++ cuda/shared.cu
@@ -0,0 +1,20 @@
+#include "shared.h"
+#include "../shared.h"
+
+// Selects a GPU
+void initialise_devices(int rank)
+{
+  int count = 0;
+  gpu_check(
+      cudaGetDeviceCount(&count));
+
+  int device_num = rank % count;
+#ifdef DEBUG
+  printf("rank %d selects device %d\n", rank, device_num);
+#endif
+
+  // Assume uniform distribution of devices on nodes
+  gpu_check(
+      cudaSetDevice(device_num));
+}
+
---
+++ omp3/shared.c
@@ -0,0 +1,7 @@
+#include "../shared.h"
+
+void initialise_devices(int rank)
+{
+  // Not required
+}
+
