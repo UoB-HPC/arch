138cf3710f9c3562b89e81a6e1f17e657551c9e1
Matthew Martineau <m.martineau@hotmail.co.uk>
2017-04-10 05:57:00 -0500

    Disallowing the use of ind macros

--- cuda/shared.h
+++ cuda/shared.h
@@ -4,9 +4,6 @@
 
 #define NTHREADS 128
 
-#define ind0 (ii*nx + jj)
-#define ind1 (ii*(nx+1) + jj)
-
 // http://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
 #define gpu_check(ans) { gpu_assert((ans), __FILE__, __LINE__); }
 inline void gpu_assert(cudaError_t code, const char *file, int line, bool abort=true)
