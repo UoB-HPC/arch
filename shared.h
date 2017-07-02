2c6732740ed2e957d9eb86882b48559196e24d65
Matt Martineau <m.martineau@hotmail.co.uk>
2017-07-02 11:58:41 -0600

    Added check for nan to ensure that the validation will fail correctly

--- shared.h
+++ shared.h
@@ -24,7 +24,7 @@
 #define absmin(a, b) ((fabs(a) < fabs(b)) ? (a) : (b))
 #define minmod(a, b) (samesign((a), (b)) ? (absmin((a), (b))) : (0.0))
 #define within_tolerance(a, b, eps) \
-  (fabs(a-b) <= ((fabs(a) > fabs(b) ? fabs(b) : fabs(a))*eps))
+  (!isnan(a) && !isnan(b) && fabs(a-b) <= ((fabs(a) > fabs(b) ? fabs(b) : fabs(a))*eps))
 #define kronecker_delta(a, b) (((a) == (b)) ? 1 : 0)
 #define triangle(a) ((a)*((a)+1)/2)
 
